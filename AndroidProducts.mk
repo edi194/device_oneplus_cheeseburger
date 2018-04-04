257a60b411b0f2305cb0cece76e5e8253cd52cb1
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    cheeseburger: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Open Source Project
+# Copyright (C) 2017-2019 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,4 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosip.mk
+    $(LOCAL_DIR)/aosip_cheeseburger.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosip_cheeseburger-user \
+    aosip_cheeseburger-userdebug \
+    aosip_cheeseburger-eng
